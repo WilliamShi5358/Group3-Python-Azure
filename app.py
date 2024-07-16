3ca2a264e012d7edfdbfbf301870c159ce7e3828
WilliamShi5358 <wei.shi@student.kpu.ca>
2024-07-15 20:28:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
+//import streamlit as st
+//import pandas as pd
+//import numpy as np
 
-st.header("INFO 4330 Python Streamlit App")
-st.write("hello world APP")
+//st.header("INFO 4330 Python Streamlit App")
+//st.write("hello world APP")
+
+print("<p>hello world</p>")
