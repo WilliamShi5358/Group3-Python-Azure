398a5b5da650c6dfd9f77c2d38ffcdfa05c87f14
WilliamShi5358 <williamshi5358@gmail.com>
2024-07-16 18:10:32 -0700

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+# show app title, header, button, and table
+st.header("INFO 4330 - Group 3")
+st.subheader('Python Streamlit App hooked with Azure web services')
+st.write("hello world ")
 
-st.header("INFO 4330 Python Streamlit App")
-st.write("hello world APP")
+
+if st.button('click here'):
+    st.write("show data")
+
+# catch the data from csv and return 
+def load_data():
+    data=pd.read_csv()
+    return data
+
+def filter_data(condition,currData):
+    data=currData
+    return data
