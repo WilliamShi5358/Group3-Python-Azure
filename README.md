e49a1cd7738a03f3419a9f93a8186042e9e9090b
WilliamShi5358 <williamshi5358@gmail.com>
2024-07-20 15:49:19 -0700

    modified app.py to create app structure to meet the requirements

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
-# Group3-Python-Azure
+## Group3-Python-Azure
 
 
-## Create Github Repository Group3-Python-Azure
-   git clone https://github.com/WilliamShi5358/Group3-Python-Azure.git
+### Github Repository Group3-Python-Azure
+   --  https://github.com/WilliamShi5358/Group3-Python-Azure.git
 
-   run python: streamlit run app.py
+##3 Create Azure web services
+   -- web app name : Group3-Python
+   -- public access address: https://group3-python.azurewebsites.net/
 
+### Online Development Github
+   -- Group3-python-Azure online github repository is associated with Azure web services
+   -- Sync by Actions on github   
 
+### Local Development Visual Studio Code
+   -- git clone https://github.com/WilliamShi5358/Group3-Python-Azure.git
+   -- run python: streamlit run app.py
+   -- before push updates to online github, process 'git pull origin main'
+   -- and then 
+   ---  git add <specific files> 
+   ---  git commit -m ''
+   ---  git push origin main
 
-## Create Azure web services
-   web app name : Group3-Python
-   public access address: https://group3-python.azurewebsites.net/
-
-## Online Development Github
-
-## Local Development Visual Studio Code
+### Python app development
+   -- data loadup
+   -- show data
+   -- filter data
+   -- visualazation data
--- app.py
+++ app.py
@@ -1,20 +1,42 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# show app title, header, button, and table
-st.header("INFO 4330 - Group 3")
-st.subheader('Python Streamlit App hooked with Azure web services')
-st.write("hello world ")
-
-
-if st.button('click here'):
-    st.write("show data")
 
 # catch the data from csv and return 
 def load_data():
-    data=pd.read_csv()
-    return data
+    #should catch data from oneDrive cloud, for now just use local data for demo 
+    path = './data/USA_Housing.csv'
+    df=pd.read_csv(path)
+    return df
 
-def filter_data(condition,currData):
-    data=currData
-    return data
+#create a funciton to filter the data
+def filter_data(data):
+    price_to_filter = st.slider('Home Price', min_value=1000000, max_value=2000000, value=(1000000,1200000))
+    filtered_df = data[data['Price'].between(*price_to_filter)]
+    st.dataframe(filtered_df)
+    
+def column_selector(data): 
+    areaIncome = currList['Avg. Area Income']
+    houseAge= currList['Avg. Area House Age']
+    areaPopulation= currList['Area Population']
+    houseAge_choice = st.sidebar.selectbox('Select your home age', houseAge)
+    areaIncome_choice = st.sidebar.selectbox('Select your area income:', areaIncome)
+    areaPopulation_choice = st.sidebar.selectbox('Select your area population',areaPopulation)  
+
+
+# show app title, header, button, and table
+st.header("INFO 4330 - Group 3")
+st.subheader('Python Streamlit App hooked with Azure web services')
+st.write("USA Housing Data for demo streamlit filter ")
+
+#load data
+currList= load_data()
+
+#click the button to show the list and filter
+#if st.button('click here to see all'):
+#   st.write(currList)
+   
+filter_data(currList)
+column_selector(currList)
+
+
