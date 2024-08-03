8480b7c62ad6b3e971890aa2c73dc090e0a0fca4
WilliamShi5358 <williamshi5358@gmail.com>
2024-08-02 21:37:08 -0700

    Modified streamlit bar chart

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st #pip install streamlit
 import pandas as pd    #pip install pandas
 import numpy as np     #pip install numpy
 
+
 # show app header,title
-st.subheader('INFO 4330 Group 3 Python Streamlit App Deploying by Azure web service')
-st.write("Dataset of Global YouTube Statistic CSV for demo streamlit filter ")
+st.subheader('INFO 4330 Group 3 Python Streamlit Filter App')
+st.write("Dataset of Global YouTube Statistic CSV ")
 
 #define global variable df 
 global df
@@ -21,7 +22,7 @@ def uploader_csv():
 
 #create a method to filter the data, return filtered dataframe, the argument is the pd datafram 
 #reference: https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/
-def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+def dynamic_filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     #placed at sidebar, and define the subheader, and created a sentinel as onFilter to turn on the filter function 
     st.sidebar.subheader('Filter Settings')
     onFilter = st.sidebar.checkbox("Add filters")
@@ -40,28 +41,30 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 def vitualization():
     path = './data/Global_YouTube_Statistics.csv'
     dataset =pd.read_csv(path)
+    dataset = pd.DataFrame(dataset)
+    #clean dataset
     dataset['Country'] = dataset['Country'].str.replace('india', 'India', case=False)
     #coding for visualization 
     chart1, chart2= st.columns(2)
     with chart1:
-        st.bar_chart(dataset.groupby('Country')['subscribers'].sum().sort_values(ascending=False).head(10), x_label='Country', y_label='subscribers')
+        st.bar_chart(dataset.groupby('Country')['subscribers'].sum().sort_values(ascending=False).head(10), x_label='Country', y_label='YouTube  Subscribers')
     with chart2:
-        st.bar_chart(dataset.groupby('Country')['Population'].mean().sort_values(ascending=False).head(10), x_label='Country', y_label='Population')
+       st.bar_chart(dataset.groupby('category')['subscribers'].sum().sort_values(ascending=False).head(10),x_label='Category', y_label='YouTube  Subscribers')
     
-    st.dataframe(pd.DataFrame(dataset))
-    
-    #coding for visualization 
+    st.dataframe(dataset)    
  
-  
+
 try:
+    #user uploads csv file
     df = uploader_csv()
 except Exception:
+    #if user doesn't uploads csv file, app can show the YouTube statistic dataset by default
     vitualization()
     print('please choose the csv file to upload for web app')
 
 #call datafram filter method 
 try: 
-   st.dataframe(filter_dataframe(df))
+   st.dataframe(dynamic_filter_dataframe(df))
 except Exception as e:
    print('please choose the csv file to upload for web app')
 
