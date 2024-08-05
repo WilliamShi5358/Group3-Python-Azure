efee11c9a812ad863b1853143f53f71c5f9b0e98
WilliamShi5358 <williamshi5358@gmail.com>
2024-08-05 11:03:19 -0700

    modified app.py and added dynamic_filter.py to meet requirements

--- app.py
+++ app.py
@@ -2,69 +2,41 @@ import streamlit as st #pip install streamlit
 import pandas as pd    #pip install pandas
 import numpy as np     #pip install numpy
 
+import streamlit_app as default_filter
+import dynamic_filter as dynamic_filter
 
 # show app header,title
-st.subheader('INFO 4330 Group 3 Python Streamlit Filter App')
-st.write("Dataset of Global YouTube Statistic CSV ")
+st.subheader('INFO 4330 Group 3 Python Streamlit App Deploying by Azure web service')
+st.write("Multi Columns Streamlit filter ")
 
 #define global variable df 
-global df
+global loaded_df
 #Creating a method to upload the csv file, return df as pd dataframe
-#reference by https://docs.streamlit.io/develop/api-reference/widgets/st.file_uploader
 def uploader_csv():
     #set sidebar sub header for file uploader
     st.sidebar.subheader('Single CSV File Upload')
     #deploy streamlit file_uploader method 
-      #coding
-    # deploy pandas library to read csv file, try and except
-      #coding
-    return df
-
-#create a method to filter the data, return filtered dataframe, the argument is the pd datafram 
-#reference: https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/
-def dynamic_filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    #placed at sidebar, and define the subheader, and created a sentinel as onFilter to turn on the filter function 
-    st.sidebar.subheader('Filter Settings')
-    onFilter = st.sidebar.checkbox("Add filters")
-
-    if not onFilter:
-        return df
-
-    df = df.copy()
-    #  coding
-    #  coding
-    #  coding
-
-    return df
-
-
-def vitualization():
-    path = './data/Global_YouTube_Statistics.csv'
-    dataset =pd.read_csv(path)
-    dataset = pd.DataFrame(dataset)
-    #clean dataset
-    dataset['Country'] = dataset['Country'].str.replace('india', 'India', case=False)
-    #coding for visualization 
-    chart1, chart2= st.columns(2)
-    with chart1:
-        st.bar_chart(dataset.groupby('Country')['subscribers'].sum().sort_values(ascending=False).head(10), x_label='Country', y_label='YouTube  Subscribers')
-    with chart2:
-       st.bar_chart(dataset.groupby('category')['subscribers'].sum().sort_values(ascending=False).head(10),x_label='Category', y_label='YouTube  Subscribers')
+    upload_file= st.sidebar.file_uploader(label='Choose csv file to upload',type=['csv'])
+    # deploy pandas library to read csv file
+    if upload_file is not None:
+        try:
+            loaded_df= pd.read_csv(upload_file)
+            st.sidebar.success("Loaded CSV dataset!")
+        except Exception:
+            print("please choose the csv file to upload...")
+    return loaded_df
     
-    st.dataframe(dataset)    
- 
-
 try:
     #user uploads csv file
-    df = uploader_csv()
+    loaded_df = uploader_csv()
 except Exception:
     #if user doesn't uploads csv file, app can show the YouTube statistic dataset by default
-    vitualization()
+    default_filter.filter()
     print('please choose the csv file to upload for web app')
 
 #call datafram filter method 
 try: 
-   st.dataframe(dynamic_filter_dataframe(df))
+   st.dataframe(dynamic_filter.dynamicFilter(loaded_df))
 except Exception as e:
    print('please choose the csv file to upload for web app')
 
